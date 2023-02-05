3d6e92174d442e492052e5fabbbcb2b36c9a5803
Metzger-PC <henry.metzgermeister@outlook.com>
2023-02-05 17:42:31 +0100

    added tests

---
+++ Moravia.Homework.Tests/Domain/Deserializer/XmlFormatDeserializerTests.cs
@@ -0,0 +1,65 @@
+﻿using AutoFixture;
+
+using FluentAssertions;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using Moravia.Homework.Domain.Deserializer;
+using Moravia.Homework.Domain.Types;
+
+using Xunit;
+
+namespace Moravia.Homework.Tests.Domain.Deserializer;
+public class XmlFormatDeserializerTests
+{
+    private readonly XmlFormatDeserializer xmlDeserializer;
+    private readonly ILogger<XmlFormatDeserializer> logger = Mock.Of<ILogger<XmlFormatDeserializer>>();
+    private readonly Fixture fixture = new();
+
+    public XmlFormatDeserializerTests()
+    {
+        xmlDeserializer = new(logger);
+    }
+
+    [Fact]
+    public void Deserialize_OK()
+    {
+        var title = fixture.Create<string>();
+        var text = fixture.Create<string>();
+
+        var input = @$"
+<?xml version=""1.0"" encoding=""utf-16""?>
+<Document xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+  <Title>{title}</Title>
+  <Text>{text}</Text>
+</Document>";
+
+        var expected = new Document { Title = title, Text = text };
+
+        var actual = xmlDeserializer.Deserialize<Document>(input);
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Deserialize_EmptyInput_ShouldReturnNull()
+    {
+        var input = string.Empty;
+
+        var actual = xmlDeserializer.Deserialize<Document>(input);
+
+        actual.Should().BeNull();
+    }
+
+    [Fact]
+    public void Deserialize_InvalidXml_ShouldReturnNull()
+    {
+        var input = "invalid xml right here";
+
+        var actual = xmlDeserializer.Deserialize<Document>(input);
+
+        actual.Should().BeNull();
+    }
+}
---
+++ Moravia.Homework.Tests/Services/SerializerServiceTests.cs
@@ -0,0 +1,90 @@
+﻿using AutoFixture;
+
+using FluentAssertions;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using Moravia.Homework.Domain.Interfaces;
+using Moravia.Homework.Domain.Types;
+using Moravia.Homework.Services;
+
+using Xunit;
+
+namespace Moravia.Homework.Tests.Services;
+public class SerializerServiceTests
+{
+    private readonly SerializerService serializerService;
+    private readonly ILogger<SerializerService> logger = Mock.Of<ILogger<SerializerService>>();
+    private readonly Mock<IFileSaverFactory> fileSaverFactoryMock = new();
+    private readonly Mock<IFileSaver> fileSaverMock = new();
+    private readonly Mock<ISerializerFactory> serializerFactoryMock = new();
+    private readonly Mock<ISerializer> serializerMock = new();
+
+    private readonly Fixture fixture = new();
+
+    public SerializerServiceTests()
+    {
+        serializerService = new(logger, serializerFactoryMock.Object, fileSaverFactoryMock.Object);
+    }
+
+    [Fact]
+    public void Serialize_OK()
+    {
+        var data = fixture.Create<Document>();
+        var context = fixture.Create<SerializationContext>();
+
+        var serializedData = fixture.Create<string>();
+
+        fileSaverFactoryMock.Setup(x => x.Create(It.IsAny<LocationType>())).Returns(fileSaverMock.Object);
+        serializerFactoryMock.Setup(x => x.Create(It.IsAny<FileFormat>())).Returns(serializerMock.Object);
+        serializerMock.Setup(x => x.Serialize(It.IsAny<Document>())).Returns(serializedData);
+
+        var succ = serializerService.Serialize(data, context);
+
+        succ.Should().BeTrue();
+        serializerFactoryMock.Verify(x => x.Create(context.Format), Times.Once);
+        serializerMock.Verify(x => x.Serialize(data), Times.Once);
+        fileSaverFactoryMock.Verify(x => x.Create(context.Location), Times.Once);
+        fileSaverMock.Verify(x => x.SaveFileFromString(context.FileName, serializedData), Times.Once);
+    }
+
+    [Fact]
+    public void Serialize_FailedSave_ShouldReturnFalse()
+    {
+        var data = fixture.Create<Document>();
+        var context = fixture.Create<SerializationContext>();
+
+        fileSaverFactoryMock.Setup(x => x.Create(It.IsAny<LocationType>())).Returns(fileSaverMock.Object);
+        fileSaverMock.Setup(x => x.SaveFileFromString(It.IsAny<string>(), It.IsAny<string>())).Returns(false);
+
+        var actual = serializerService.Serialize(data, context);
+        
+        actual.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Serialize_NoData_ShouldReturnFalse()
+    {
+        var data = null as Document;
+        var context = fixture.Create<SerializationContext>();
+
+        var succ = serializerService.Serialize(data, context);
+
+        succ.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Serialize_NoContext_ShouldThrowArgumentNullException()
+    {
+        var data = fixture.Create<Document>();
+        var context = null as SerializationContext;
+
+#pragma warning disable CS8604 // intentional null reference argument 
+        var action = () => serializerService.Serialize(data, context);
+#pragma warning restore CS8604 
+
+        action.Should().Throw<ArgumentNullException>();
+    }
+}
