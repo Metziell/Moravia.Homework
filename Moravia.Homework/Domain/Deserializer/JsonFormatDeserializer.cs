3b6c269674161459b141c774f261a06c03ba51a5
Metzger-PC <henry.metzgermeister@outlook.com>
2023-02-05 17:41:51 +0100

    added exception handling to deserializers

--- Moravia.Homework/Domain/Deserializer/JsonFormatDeserializer.cs
+++ Moravia.Homework/Domain/Deserializer/JsonFormatDeserializer.cs
@@ -1,10 +1,19 @@
-﻿using Moravia.Homework.Domain.Interfaces;
+﻿using Microsoft.Extensions.Logging;
+
+using Moravia.Homework.Domain.Interfaces;
 
 using Newtonsoft.Json;
 
 namespace Moravia.Homework.Domain.Deserializer;
 public class JsonFormatDeserializer : IDeserializer
 {
+    private readonly ILogger<JsonFormatDeserializer> logger;
+
+    public JsonFormatDeserializer(ILogger<JsonFormatDeserializer> logger)
+    {
+        this.logger = logger;
+    }
+
     public T? Deserialize<T>(string data)
     {
         if (string.IsNullOrWhiteSpace(data))
@@ -12,6 +21,14 @@ public class JsonFormatDeserializer : IDeserializer
             return default;
         }
 
-        return JsonConvert.DeserializeObject<T>(data);
+        try
+        {
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+        catch (JsonReaderException ex)
+        {
+            logger.LogError(ex, "Failed to deserialize JSON {data}", data);
+            return default;
+        }
     }
 }
--- Moravia.Homework/Domain/Deserializer/XmlFormatDeserializer.cs
+++ Moravia.Homework/Domain/Deserializer/XmlFormatDeserializer.cs
@@ -1,10 +1,19 @@
 ﻿using System.Xml.Serialization;
 
+using Microsoft.Extensions.Logging;
+
 using Moravia.Homework.Domain.Interfaces;
 
 namespace Moravia.Homework.Domain.Deserializer;
 public class XmlFormatDeserializer : IDeserializer
 {
+    private readonly ILogger<XmlFormatDeserializer> logger;
+
+    public XmlFormatDeserializer(ILogger<XmlFormatDeserializer> logger)
+    {
+        this.logger = logger;
+    }
+
     public T? Deserialize<T>(string data)
     {
         if (string.IsNullOrWhiteSpace(data))
@@ -15,6 +24,14 @@ public class XmlFormatDeserializer : IDeserializer
         var serializer = new XmlSerializer(typeof(T));
         using var reader = new StringReader(data.Trim());
 
-        return (T?)serializer.Deserialize(reader);
+        try
+        {
+            return (T?)serializer.Deserialize(reader);
+        }
+        catch (InvalidOperationException ex)
+        {
+            logger.LogError(ex, "Failed to deserialize XML {data}", data);
+            return default;
+        }
     }
 }
